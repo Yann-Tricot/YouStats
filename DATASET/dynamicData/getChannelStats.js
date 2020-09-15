98d64f66e1737d2331fb92030969b4c075a0c029
Yann Tricot <tricot.yann@gmail.com>
2020-09-15 19:40:28 +0200

    V1.01 - Improve fetch API YT

--- DATASET/dynamicData/getChannelStats.js
+++ DATASET/dynamicData/getChannelStats.js
@@ -1,13 +1,11 @@
 const API_KEY = 'AIzaSyDn3jBs7OUSc0HaAzj23ee8fVdexO06kpg'
 const CHANNEL_ID = 'UCcpxvJLWnMj5c70D984Bd-Q'
 
-const getChannelStats = () => {
-    fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${CHANNEL_ID}&key=${API_KEY}`)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-    })
+async function getChannelStats(){
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${CHANNEL_ID}&key=${API_KEY}`)
+    const data = await response.json()
+    console.log('DATA: ', data)
 }
-getChannelStats();
+
+//ASYNC FUNCTION CALL
+getChannelStats()
