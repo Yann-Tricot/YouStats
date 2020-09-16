4c9105ca486e90de678942880e6b1e50508a9275
Yann Tricot <tricot.yann@gmail.com>
2020-09-16 11:54:32 +0200

    V1.00 - ADD getCategories code with a YT API fetch

---
+++ DATASET/dynamicData/getCategories.js
@@ -0,0 +1,18 @@
+const API_KEY = 'AIzaSyDn3jBs7OUSc0HaAzj23ee8fVdexO06kpg'
+const REGION_CODE = 'US'
+
+const URL = `https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=${REGION_CODE}&key=${API_KEY}`
+
+const CATEGORIES_ARRAY = []
+
+async function getCategories(){
+    const response = await fetch(URL)
+    const data = await response.json()
+    const CATEGORIES_DATA = data
+    return CATEGORIES_DATA
+}
+
+//ASYNC FUNCTION CALL
+getCategories().then((CATEGORIES_DATA) => {
+    CATEGORIES_ARRAY.push(CATEGORIES_DATA.items)
+})
