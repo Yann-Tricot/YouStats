d29f08ea3adf72f417b3f4324e0b0ef7a8c89738
Alexandre LAVAL <herstalex@gmail.com>
2020-09-22 18:21:08 +0200

    v1.2 Creation VideoController, videorepository (gestion des entity pour l'affichage sur la page country) Ajout indexChannel.html.twig(affichage des channels)

---
+++ src/Entity/Tag.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tag
+ *
+ * @ORM\Table(name="tag")
+ * @ORM\Entity
+ */
+class Tag
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="tag_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $tagId;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="name", type="string", length=1000, nullable=true, options={"default"="NULL"})
+   #  * @ORM\Column(name="name", type="string", length=1000, nullable=true, options={"default"="NULL"})
+     */
+    private $name = 'NULL';
+    #private $name = 'NULL';
+
+    public function getTagId(){
+        return $this->tagId;
+    }
+
+    public function getTagName(){
+        return $this->name;
+    }
+}
